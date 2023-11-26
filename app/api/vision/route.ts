33badaa2ce03cd10da63537fb13fc13e99bfca87
motorcykle <mohamedabdulridha1@gmail.com>
2023-11-26 18:45:52 +0100

    finish

---
+++ app/api/vision/route.ts
@@ -0,0 +1,49 @@
+// import { Configuration, OpenAIApi } from "openai";
+// const configuration = new Configuration({
+//     organization: "org-KLBa9GyUcmGMbmmOCYBdW9LK",
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+import { NextResponse } from "next/server"
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI });
+
+export async function POST(request: Request) {
+
+    const data = await request.json();
+
+    if (data?.image) {
+        console.log(data)
+        try {
+            const response = await openai.chat.completions.create({
+                model: "gpt-4-vision-preview",
+                messages: [
+                    {
+                        role: "user",
+                        content: [
+                        { type: "text", text: data?.prompt || "What’s in this image?" },
+                        {
+                            type: "image_url",
+                            image_url: {
+                            "url": data.image,
+                            },
+                        },
+                        ],
+                    },
+                ],
+                max_tokens: 300
+            });
+    
+            // console.log(response.choices[0]);
+            return NextResponse.json({ response: response.choices[0]}, { status: 200 })
+        } catch (error) {
+            return NextResponse.json({ error }, { status: 400 })
+        }
+        
+    }
+    
+    return NextResponse.json({ message: 'Received' }, { status: 400 })
+}
